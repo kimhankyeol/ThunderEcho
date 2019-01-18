eae5a50903d4e9fcc3279f556857059016524260
jinsusong <iko153@naver.com>
2019-01-18 14:38:56 +0900

    2019/01/18 song master h

--- ThunderEcho/src/poly/controller/IntroController.java
+++ ThunderEcho/src/poly/controller/IntroController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IntroController {
 	private Logger log = Logger.getLogger(this.getClass());
 	
+	//회사소개
 	@RequestMapping(value="introduction/intro")
 	public String intro(HttpServletRequest req, HttpServletResponse res) throws Exception {
 		log.info(this.getClass() + " intro company Start!!!");
@@ -18,10 +19,20 @@ public class IntroController {
 		return "/introduction/intro";
 	}
 	
+	//조직도
 	@RequestMapping(value="introduction/groupChart")
 	public String groupChart() throws Exception{
 		log.info(this.getClass() + "groupChart Start!!!");
 		log.info(this.getClass() + "groupChart End!!!");
 		return "/introduction/groupChart";
 	}
+	
+	//오시는길
+		@RequestMapping(value="introduction/road")
+		public String road() {
+			log.info(this.getClass() + " .introdection/road Start ~~");
+			
+			log.info(this.getClass() + " .introdection/road end ~~");
+			return "/introduction/road";
+		}
 }
