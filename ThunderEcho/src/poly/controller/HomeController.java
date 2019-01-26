549939aae0666128fc7b680b3f19490ce312726f
kimhankyeol <kimhk1030@naver.com>
2019-01-27 04:30:44 +0900

    2019-01-27 04:30 홈컨트롤러추가 & top.jsp add

---
+++ ThunderEcho/src/poly/controller/HomeController.java
@@ -0,0 +1,18 @@
+package poly.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping(value="main")
+	public String main() throws Exception{
+		
+		System.out.println("main");
+		return "/index";
+	}
+}
